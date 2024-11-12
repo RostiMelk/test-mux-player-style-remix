64a73406e50d4ea0e2f80ee7b144772b7e08843b
RostiMelk <hello@rosti.no>
2024-11-12 10:16:08 +0100

    feat: remix label

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
   return (
     <main className="flex flex-col gap-8 items-center justify-center h-screen container mx-auto">
       <h1 className="text-4xl font-bold text-center">
-        Mux Video player.style test
+        Mux Video player.style test (Remix)
       </h1>
 
       <MuxTheme
